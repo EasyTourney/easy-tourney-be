6e4702edbca81cb639364896eefe7aeae21f1ca8
Khánh Ngô <khanh.duc.quoc.ngo@mgm-tp.com>
2023-12-20 11:44:42 +0000

    MGMIDN 997: edit team and delete team in a tournament
    Merge in DA-NANG-INTERNSHIP/easy-tourney-be from feature/MGMIDN-997-edit-team-in-a-tournament-be to master
    
    Squashed commit of the following:
    
    commit 4edbbd1eafda0e58eeeec2efc113cb41d914a854
    Author: Quoc Khanh <nguyenkimt322@gmail.com>
    Date:   Wed Dec 20 17:04:01 2023 +0700
    
        MGMIDN 997: edit team anđ delete team in a tournament

--- src/main/java/com/example/easytourneybe/team/TeamController.java
+++ src/main/java/com/example/easytourneybe/team/TeamController.java
@@ -1,33 +1,33 @@
 package com.example.easytourneybe.team;
 
-import com.example.easytourneybe.category.Category;
 import com.example.easytourneybe.model.ResponseObject;
 import com.example.easytourneybe.team.dto.TeamPlayerDto;
-import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
+import java.util.Optional;
+
 import static com.example.easytourneybe.constants.DefaultListParams.PAGE;
 import static com.example.easytourneybe.constants.DefaultListParams.SIZE;
 
 
 @RestController
 @CrossOrigin
-@RequestMapping("/tournament")
+@RequestMapping("/tournament/{tournament_id}")
 public class TeamController {
     @Autowired
-    private TeamService TeamService;
-    @GetMapping("/{id}/team")
+    private TeamService teamService;
+    @GetMapping("/team")
     public ResponseEntity<ResponseObject> findAllTeamAndPlayer(
-            @PathVariable Integer id ,
+            @PathVariable Integer tournament_id ,
             @RequestParam(defaultValue = SIZE) Integer size,
             @RequestParam(defaultValue = PAGE) Integer page
     ) {
-        Long totalTeamRecords = TeamService.getTotalRecordsForTournament(id);
-        List<TeamPlayerDto> teamAndPlayer = TeamService.getAllTeamAndPlayerCount(id, page-1,size);
+        Long totalTeamRecords = teamService.getTotalRecordsForTournament(tournament_id);
+        List<TeamPlayerDto> teamAndPlayer = teamService.getAllTeamAndPlayerCount(tournament_id, page-1,size);
         ResponseObject responseObject = new ResponseObject(
                 true, teamAndPlayer.size(), teamAndPlayer
         );
@@ -36,12 +36,26 @@ public class TeamController {
                 responseObject
         );
     }
-    @PostMapping("/{id}/team")
+    @PostMapping("/team")
     public ResponseEntity<ResponseObject> createTeam(@Valid @RequestBody Team team,
-                                                     @PathVariable Integer id) {
-        Team temp = TeamService.createTeam(team.getTeamName().trim(), id);
+                                                     @PathVariable Integer tournament_id) {
+
+        Team temp = teamService.createTeam(team.getTeamName().trim(), tournament_id);
         return ResponseEntity.status(HttpStatus.CREATED).body(
                 new ResponseObject(true, 1, temp)
         );
+    }
+    @PutMapping("/team/{id}")
+    public ResponseEntity<ResponseObject> updateTeam(@PathVariable Integer tournament_id, @PathVariable Integer id, @Valid @RequestBody Team team) {
+        Optional<Team> updateTeam = teamService.updateTeam(tournament_id, id, team.getTeamName().trim());
+        return ResponseEntity.status(HttpStatus.OK).body(
+                new ResponseObject(true, 1, updateTeam)
+        );
+    }
+    @DeleteMapping("/team/{id}")
+    public ResponseEntity<ResponseObject> deleteTeam(@PathVariable Integer tournament_id, @PathVariable Integer id) {
+        Optional<Team> deleteTeam = teamService.deleteTeam(tournament_id, id);
+        return ResponseEntity.status(HttpStatus.OK).body(
+                new ResponseObject(true, 1, deleteTeam));
     }
 }
--- src/main/java/com/example/easytourneybe/team/TeamService.java
+++ src/main/java/com/example/easytourneybe/team/TeamService.java
@@ -3,11 +3,11 @@ package com.example.easytourneybe.team;
 import com.example.easytourneybe.exceptions.InvalidRequestException;
 import com.example.easytourneybe.team.dto.TeamPlayerDto;
 import com.example.easytourneybe.team.interfaces.TeamRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-
 import java.time.LocalDateTime;
 import java.util.*;
 import java.util.stream.Collectors;
@@ -24,19 +24,49 @@ public class TeamService {
                    .collect(Collectors.toList());
 
    }
-    public boolean hasExistTeamName(String teamName) {
-        return !teamRepository.findTeamsByName(teamName.trim()).isEmpty();
+    public boolean hasExistTeamName(Integer tournamentId, String teamName) {
+        return !teamRepository.findTeamsByName(tournamentId, teamName.trim()).isEmpty();
     }
-    public Team createTeam (String teamName, Integer id) {
+    public Team createTeam (String teamName, Integer tournamentId) {
 
-        if (hasExistTeamName(teamName.trim())) {
+        if (hasExistTeamName(tournamentId, teamName.trim())) {
             throw new InvalidRequestException("Team name has already existed");
         }
         Team team = new Team();
-        team.setTournamentId(id);
+        team.setTournamentId(tournamentId);
         team.setTeamName(teamName.trim());
         team.setCreatedAt(LocalDateTime.now());
         return teamRepository.save(team);
+    }
+    public Optional<Team> updateTeam(Integer tournamentId, Integer id, String teamName) {
+        Optional<Team> teamOptional = teamRepository.findTeamById(tournamentId, id);
+
+        if(hasExistTeamName(tournamentId, teamName.trim())){
+            throw new InvalidRequestException("Team name has already exist");
+        }
+
+        if (teamOptional.isPresent()) {
+            Team team = teamOptional.get();
+            team.setTeamName(teamName);
+            team.setUpdatedAt(LocalDateTime.now());
+            team.setTournamentId(tournamentId);
+            teamRepository.save(team);
+            return Optional.of(team);
+        } else {
+            throw new NoSuchElementException("Team not found");
+        }
+    }
+    @Transactional
+    public Optional<Team> deleteTeam(Integer tournamentId, Integer id) {
+        Optional<Team> teamOptional = teamRepository.findTeamById(tournamentId,id);
+
+        if (teamOptional.isPresent()) {
+            Team team = teamOptional.get();
+            teamRepository.deleteByTournamentIdAndTeamId(tournamentId, id);
+            return Optional.of(team);
+        } else {
+            throw new NoSuchElementException("Category not found");
+        }
     }
         public long getTotalRecordsForTournament(Integer id) {
             return teamRepository.getTotalRecordsForTournament(id);
